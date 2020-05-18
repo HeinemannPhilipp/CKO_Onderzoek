25cae33c4972f1b3e3a15f074c5f058c65e9f5a3
HeinemannPhilipp <65544923+HeinemannPhilipp@users.noreply.github.com>
2020-05-18 14:23:54 +0200

    Update Tekenvoorbeeld2.py

--- Tekenvoorbeeld2.py
+++ Tekenvoorbeeld2.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 
 
 @jit
-def mandelbrot(Re, lm, max_iter):
-    c = complex(Re, lm)
+def mandelbrot(Re, Im, max_iter):
+    c = complex(Re, Im)
     z = 0.0j
 
     for i in range(max_iter):
-        z * z*z + c
+        z = z*z + c
         if (z.real*z.real + z.imag*z.imag) >= 4:
             return i
     return max_iter
@@ -20,12 +20,12 @@ rows = 5000
 
 result = np.zeros([rows, columns])
 for row_index, Re in enumerate(np.linspace(-2, 1, num=rows)):
-    for column_index, lm in enumerate(np.linspace(-1, 1, num=columns)):
-        result[row_index, column_index] = mandelbrot(Re, lm, 500)
+    for column_index, Im in enumerate(np.linspace(-1, 1, num=columns)):
+        result[row_index, column_index] = mandelbrot(Re, Im, 500)
 
 
 plt.figure(dpi=100)
 plt.imshow(result.T, cmap='copper', interpolation='bilinear', extent=[-2, 1, -1, 1])
 plt.xlabel('Re')
-plt.ylabel('lm')
+plt.ylabel('Im')
 plt.show()
